766ae9e10ed8144cfa4137224860c1ad7a1818d4
风岚 <1837115857@qq.com>
2026-07-22 22:36:25 +0800

    feat: 添加统一成功响应边界

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,5 +1,8 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { APP_INTERCEPTOR } from "@nestjs/core";
 import { AuthModule } from "./auth/auth.module";
+import { ApiResponseInterceptor } from "./common/http/api-response.interceptor";
+import { RequestIdMiddleware } from "./common/http/request-id.middleware";
 import { ConfigModule } from "./config/config.module";
 import { HealthModule } from "./health/health.module";
 import { OrderModule } from "./modules/order/order.module";
@@ -8,5 +11,10 @@ import { PrismaModule } from "./prisma/prisma.module";
 
 @Module({
   imports: [ConfigModule, PrismaModule, AuthModule, HealthModule, UserModule, OrderModule],
+  providers: [{ provide: APP_INTERCEPTOR, useClass: ApiResponseInterceptor }],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(RequestIdMiddleware).forRoutes("*");
+  }
+}
