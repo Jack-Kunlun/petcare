1f3ee464a1a824aaad0b2269febf1e0ac4dc0594
风岚 <1837115857@qq.com>
2026-08-11 23:50:31 +0800

    refactor(uniapp): 收敛为最小业务壳

--- apps/uniapp/manifest.config.ts
+++ apps/uniapp/manifest.config.ts
@@ -1,12 +1,3 @@
-/*
- * @Author: weisheng
- * @Date: 2025-08-28 20:59:43
- * @LastEditTime: 2025-11-17 14:28:09
- * @LastEditors: weisheng
- * @Description:
- * @FilePath: /wot-starter/manifest.config.ts
- * 记得注释
- */
 import { defineManifestConfig } from "@uni-helper/vite-plugin-uni-manifest";
 
 export default defineManifestConfig({
@@ -15,8 +6,6 @@ export default defineManifestConfig({
   description: "",
   versionName: "1.0.0",
   versionCode: "100",
-  transformPx: false,
-  /* 5+App特有相关 */
   "app-plus": {
     usingComponents: true,
     nvueStyleCompiler: "uni-app",
@@ -27,21 +16,13 @@ export default defineManifestConfig({
       autoclose: true,
       delay: 0,
     },
-    /* 模块配置 */
     modules: {},
-    /* 应用发布信息 */
     distribute: {
-      /* Android permissions are added only when a PetCare feature requires them. */
       android: {},
-      /* ios打包配置 */
       ios: {},
-      /* SDK配置 */
       sdkConfigs: {},
     },
   },
-  /* 快应用特有相关 */
-  quickapp: {},
-  /* 小程序特有相关 */
   "mp-weixin": {
     optimization: {
       subPackages: true,
@@ -51,28 +32,8 @@ export default defineManifestConfig({
       urlCheck: true,
     },
     usingComponents: true,
-    darkmode: true,
-    themeLocation: "theme.json",
   },
-  "app-harmony": {},
-  "mp-harmony": {},
-  "mp-alipay": {
-    usingComponents: true,
-    compileOptions: {
-      globalObjectMode: "enable",
-      treeShaking: true,
-    },
-  },
-  "mp-baidu": {
-    usingComponents: true,
-  },
-  "mp-toutiao": {
-    usingComponents: true,
-  },
-  h5: {
-    darkmode: true,
-    themeLocation: "theme.json",
-  },
+  h5: {},
   uniStatistics: {
     enable: false,
   },
